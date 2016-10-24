4a5d83755abee7a85bd756099e175449fc60c6fb
jhersonos <jherson.o.s@hotmail.com>
2016-10-24 14:04:53 -0500

    change to direction
    service workes

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const cache_name="ya cache v1"
-const cache_urls = ["offline/view.html","/frontend/offline/style.css","/frontend/offline/off.png"]
+const cache_name="ya cache v2"
+const cache_urls = ["/offline/view.html","/offline/style.css","/offline/off.png"]
 
 self.addEventListener("install",function(ev){
 	console.log("ServiceWorkers instalado")
